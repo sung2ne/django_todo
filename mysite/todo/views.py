fa3768e0fc962665712224af3627962f207c3de5
정필성 <ibetter.kr@gmail.com>
2024-07-25 12:50:20 +0900

    기능 수정

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -5,10 +5,15 @@ from todo.models import Todo, Item
 
 # 할일 목록, /todo/
 def index(request):
+    # 할일 목록
     todoList = Todo.objects.order_by('id')
+
+    # 전달 내용
     context = {
         "todoList": todoList,
     }
+
+    # 응답
     return render(request, "todo/index.html", context)
 
 # 할일 등록, /todo/create/
@@ -19,10 +24,7 @@ def todoCreate(request):
         todo_text = body.get('todo_text')
         
         # 데이터베이스에 할일 추가하기
-        Todo.objects.create(todo_text=todo_text)
-        
-        # Todo에서 마지막 데이터 가져오기
-        todo = Todo.objects.order_by("id").last()
+        todo = Todo.objects.create(todo_text=todo_text)
         
     # JSON 응답
     return JsonResponse({"id": todo.id, "todo_text": todo.todo_text}, status=200)
@@ -66,12 +68,42 @@ def todoDelete(request, todo_id):
 
 # 상세 항목 등록, /todo/<todo_id>/create/
 def itemCreate(request, todo_id):
-    return HttpResponse("todo, create")
+    if request.method == "POST":
+        # 받은 값
+        body = json.loads(request.body)
+        item_text = body.get('item_text')
+        
+        # 데이터베이스에 할일 추가하기
+        item = Item.objects.create(todo_id=todo_id, item_text=item_text)
+        
+    # JSON 응답
+    return JsonResponse({"id": item.id, "item_text": item.item_text}, status=200)
 
 # 상세 항목 수정, /todo/<todo_id>/<item_id>/update/ 
 def itemUpdate(request, todo_id, item_id):
-    return HttpResponse("todo, update")
+    if request.method == "POST":
+        # 받은 값
+        body = json.loads(request.body)
+        item_text = body.get('item_text')
+        
+        # 기존 상세 항목 정보
+        item = get_object_or_404(Todo, pk=item_id)
+        
+        # 상세 항목 수정
+        item.item_text = item_text
+        item.save()
+        
+    # JSON 응답
+    return JsonResponse({"id": item.id, "item_text": item.item_text}, status=200)
 
 # 상세 항목 삭제, /todo/<todo_id>/<item_id>/delete/
 def itemDelete(request, todo_id, item_id):
-    return HttpResponse("todo, delete")
+    if request.method == "POST":
+        # 기존 상세 항목 정보
+        item = get_object_or_404(Item, pk=item_id)
+        
+        # 상세 항목 삭제
+        item.delete()
+        
+    # JSON 응답
+    return JsonResponse({}, status=200)
