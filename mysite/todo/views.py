70f52d63b96dae0a70dfc6b2283cb7557b67c3a6
정필성 <ibetter.kr@gmail.com>
2024-07-25 12:05:21 +0900

    기능 수정

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,11 +1,14 @@
 import json
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from todo.models import Todo, Item
 
 # 할일 목록, /todo/
 def index(request):
-    context = {}
+    todoList = Todo.objects.order_by('id')
+    context = {
+        "todoList": todoList,
+    }
     return render(request, "todo/index.html", context)
 
 # 할일 등록, /todo/create/
@@ -19,31 +22,56 @@ def todoCreate(request):
         Todo.objects.create(todo_text=todo_text)
         
         # Todo에서 마지막 데이터 가져오기
-        todo = Todo.objects.order_by("-id").last()
+        todo = Todo.objects.order_by("id").last()
         
     # JSON 응답
     return JsonResponse({"id": todo.id, "todo_text": todo.todo_text}, status=200)
 
 # 할일 보기, /todo/<todo_id>/
-def todoDetail(request):
-    return HttpResponse("todo, detail")
+def todoDetail(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    context = {
+        "todo": todo,
+    }
+    return render(request, "todo/detail.html", context)
 
 # 할일 수정, /todo/<todo_id>/update/
-def todoUpdate(request):
-    return HttpResponse("todo, update")
+def todoUpdate(request, todo_id):
+    if request.method == "POST":
+        # 받은 값
+        body = json.loads(request.body)
+        todo_text = body.get('todo_text')
+        
+        # 기존 할일 정보
+        todo = get_object_or_404(Todo, pk=todo_id)
+        
+        # 할일 수정
+        todo.todo_text = todo_text
+        todo.save()
+        
+    # JSON 응답
+    return JsonResponse({"id": todo.id, "todo_text": todo.todo_text}, status=200)
 
 # 할일 삭제, /todo/<todo_id>/delete/
-def todoDelete(request):
-    return HttpResponse("todo, delete")
+def todoDelete(request, todo_id):
+    if request.method == "POST":
+        # 기존 할일 정보
+        todo = get_object_or_404(Todo, pk=todo_id)
+        
+        # 할일 삭제
+        todo.delete()
+        
+    # JSON 응답
+    return JsonResponse({}, status=200)
 
 # 상세 항목 등록, /todo/<todo_id>/create/
-def itemCreate(request):
+def itemCreate(request, todo_id):
     return HttpResponse("todo, create")
 
 # 상세 항목 수정, /todo/<todo_id>/<item_id>/update/ 
-def itemUpdate(request):
+def itemUpdate(request, todo_id, item_id):
     return HttpResponse("todo, update")
 
 # 상세 항목 삭제, /todo/<todo_id>/<item_id>/delete/
-def itemDelete(request):
+def itemDelete(request, todo_id, item_id):
     return HttpResponse("todo, delete")
